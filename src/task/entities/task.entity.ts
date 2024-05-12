79d7af86dbc9cfd72ad77b61235e94cc69b3ae81
Edwin Gomez <endoflong987@gmail.com>
2024-05-12 16:22:23 -0500

    a

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,1 +1,22 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class Task {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    task_category_id: string;
+
+    @Column()
+    task_status_id: string;
+
+    @Column()
+    user_id: number;
+
+    @Column({ default: true })
+    isActive: boolean;
+}
+
+
