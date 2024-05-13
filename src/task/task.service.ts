9166733f886777043d998a204ffad18863a33b42
Edwin Gomez <endoflong987@gmail.com>
2024-05-12 22:20:02 -0500

    apicul-taskcrud done

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TaskService {
+  constructor(  @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+  ) {}
+
+
   create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+    return this.taskRepository.save(createTaskDto);
   }
 
   findAll() {
-    return `This action returns all task`;
+    return this.taskRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} task`;
+    return this.taskRepository.findOneBy({ id });
   }
 
   update(id: number, updateTaskDto: UpdateTaskDto) {
     return `This action updates a #${id} task`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: number) {
+    await this.taskRepository.delete(id);
+    return `This task has been deleted.`
   }
 }
